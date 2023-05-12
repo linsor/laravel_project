0e8e7b712ff7164b3f7aa5675f64018f0039d225
linsor <65354540+linsor@users.noreply.github.com>
2023-05-12 21:23:31 +0600

    Create new View file, and Controllers
    1. About view and controoler;
    2. Contacts view and controller;
    3. Main view and controoler;

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/posts/update', [PostController::class, 'update']);
 Route::get('/posts/delete', [PostController::class, 'delete']);
 Route::get('/posts/first_or_create', [PostController::class, 'firstOrCreate']);
 Route::get('/posts/update_or_create', [PostController::class, 'updateOrCreate']);
+
+Route::get('/main', [MainController::class, 'index']);
+Route::get('/contacts', [ContactsController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
