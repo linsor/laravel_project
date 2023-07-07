5d13c419982e0ac2bfcdcc2fce6773be6db9a78c
linsor <65354540+linsor@users.noreply.github.com>
2023-07-07 12:43:27 +0600

    Create Requests

---
+++ app/Http/Controllers/Post/CreateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+
+
+class CreateController extends Controller
+{
+    public function __invoke() {
+        
+        $tags = Tag::all();
+        $categories = Category::all();
+        return view('post/create', compact('categories' , 'tags'));
+    }
+}
+
---
+++ app/Http/Controllers/Post/StoreController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
+use App\Models\Post;
+use Illuminate\Auth\Events\Validated;
+
+class StoreController extends Controller
+{
+    public function __invoke(StoreRequest $request) {
+        
+        $data = $request->validated();
+        $tags = $data['tags'];
+        unset($data['tags']);
+        $post= Post::create($data);
+        $post->tags()->attach($tags);
+        return redirect()->route('post.index');
+    }
+}
+
